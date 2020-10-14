5149982e55a0ec79d602c69cbcf336af12bbdc2f
Daniel Fox <danielfox@thoughtstoodefinite.com>
2020-10-14 11:33:17 -0400

    added count of census tracts in CBSA to population.py

--- src/population.py
+++ src/population.py
@@ -1,4 +1,5 @@
 import csv
+from collections import defaultdict
 import population_funcs
 
 
@@ -6,8 +7,7 @@ import population_funcs
 path_to_csv = '../input/censustract-00-10.csv'
 cols, cols_inds = population_funcs.create_column_dicts(path_to_csv)
 
-select_cols= ['GEOID', 'ST10', 'COU10', 'TRACT10', 'CBSA09', 'CBSA_T', 'POP00', 'POP10', 'PPCHG']
-
+select_cols= ['GEOID', 'CBSA09', 'CBSA_T', 'POP00', 'POP10', 'PPCHG']
 
 
 # population_funcs.print_select_columns(select_cols, cols, cols_inds)
@@ -16,3 +16,17 @@ select_cols= ['GEOID', 'ST10', 'COU10', 'TRACT10', 'CBSA09', 'CBSA_T', 'POP00', 
 # print(num_rows)
 
 # population_funcs.write_col_keys(cols)	
+
+def count_tracts(path_to_csv, select_cols):
+	tract_count = defaultdict(int)
+	with open(path_to_csv, newline = '') as csvfile:
+		censusreader = csv.reader(csvfile, delimiter = ',')
+		row = next(censusreader)
+		for row in censusreader:
+			tract_count[row[cols_inds['CBSA09']]] +=1	
+	return dict(tract_count)
+
+path_to_test_1 = '../insight_testsuite/tests/test_1/input/censustract-00-10.csv'
+tract_count = count_tracts(path_to_test_1, select_cols)
+print(tract_count.items())
+print(tract_count['28540'])
