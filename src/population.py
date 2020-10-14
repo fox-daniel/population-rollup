53b239313dfbc06db1c9765747464976db493fef
Daniel Fox <danielfox@thoughtstoodefinite.com>
2020-10-14 12:15:26 -0400

    added ppchg avg to gropuby func

--- src/population.py
+++ src/population.py
@@ -21,6 +21,7 @@ def groupby_cbsa(path_to_csv, select_cols):
 	tract_count = defaultdict(int)
 	pop00_count = defaultdict(int)
 	pop10_count = defaultdict(int)
+	ppchg_avg = defaultdict(int)
 	with open(path_to_csv, newline = '') as csvfile:
 		censusreader = csv.reader(csvfile, delimiter = ',')
 		row = next(censusreader)
@@ -28,10 +29,14 @@ def groupby_cbsa(path_to_csv, select_cols):
 			tract_count[row[cols_inds['CBSA09']]] += 1
 			pop00_count[row[cols_inds['CBSA09']]] += int(row[cols_inds['POP00']])
 			pop10_count[row[cols_inds['CBSA09']]] += int(row[cols_inds['POP10']])
-	return dict(tract_count), dict(pop00_count), dict(pop10_count)
+			ppchg_avg[row[cols_inds['CBSA09']]] += float(row[cols_inds['PPCHG']])
+		for k, v in ppchg_avg.items():
+			ppchg_avg[k] = round(ppchg_avg[k]/tract_count[k],2)
+	return dict(tract_count), dict(pop00_count), dict(pop10_count), dict(ppchg_avg) 
 
 path_to_test_1 = '../insight_testsuite/tests/test_1/input/censustract-00-10.csv'
-tract_count, pop00_count, pop10_count = groupby_cbsa(path_to_csv, select_cols)
+tract_count, pop00_count, pop10_count, ppchg_avg = groupby_cbsa(path_to_test_1, select_cols)
 print(tract_count.items())
 print(pop00_count.items())
 print(pop10_count.items())
+print(ppchg_avg.items())
