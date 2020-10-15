6d241d32c7233378410adcc06a39d0192aaecf48
Daniel Fox <danielfox@thoughtstoodefinite.com>
2020-10-15 14:30:26 -0400

    add geoid concat validation to pipeline_0

--- src/pipeline_0.py
+++ src/pipeline_0.py
@@ -1,8 +1,12 @@
 import csv
 import importlib
-import pipeline_funcs
-importlib.reload(pipeline_funcs)
+import development_funcs
+import validation_funcs
+import transformation_funcs
 
+importlib.reload(validation_funcs)
+importlib.reload(transformation_funcs)
+importlib.reload(development_funcs)
 
 path_to_raw = "../input/censustract-00-10.csv"
 path_to_log_validate_0 = "./log_validate_0.csv"
@@ -12,7 +16,9 @@ path_to_test_1 = "../insight_testsuite/tests/test_1/input/censustract-00-10.csv"
 path_to_report = "../output/report.csv"
 
 # dictionaries {column_index, column_name} & {column_name, column_index}
-cols, cols_inds = pipeline_funcs.create_column_dicts(path_to_raw)
+cols, cols_inds = transformation_funcs.create_column_dicts(path_to_raw)
 
 select_cols = ["GEOID", "CBSA09", "CBSA_T", "POP00", "POP10", "PPCHG"]
 
+
+validation_funcs.test_geoid_concat(path_to_raw, path_to_log_validate_0, cols, cols_inds)
--- src/validation_funcs.py
+++ src/validation_funcs.py
@@ -1,20 +1,22 @@
 import csv
 
-def test_code_concat(path_to_csv, cols, cols_inds):
+def test_geoid_concat(path_to_csv, path_to_log, cols, cols_inds):
 	cols_concat = ['GEOID', 'ST10', 'COU10', 'TRACT10', 'CBSA09', 'CBSA_T', 'POP00']
 
 	with open(path_to_csv, newline = '') as csvfile:
 		censusreader = csv.reader(csvfile, delimiter = ',')
-		i = 0
-		# move to the first row with data
-		row = next(censusreader)
-		for row in censusreader:
-			new_row = [row[cols_inds['GEOID']],\
-			row[cols_inds['ST10']]+row[cols_inds['COU10']]+row[cols_inds['TRACT10']]]
-			if new_row[0] != new_row[1]:
-				print(False)
-			i += 1
-		print('final row checked: ',i)
+		with open(path_to_log, mode = "w", newline = "") as logfile:
+			geowriter = csv.writer(logfile, delimiter=",")
+			i = 0
+			# move to the first row with data
+			row = next(censusreader)
+			for row in censusreader:
+				new_row = [row[cols_inds['GEOID']],\
+				row[cols_inds['ST10']]+row[cols_inds['COU10']]+row[cols_inds['TRACT10']]]
+				if new_row[0] != new_row[1]:
+					geowriter.writerow(row)
+				i += 1
+			geowriter.writerow(['final row checked: {0}'.format(i)])
 
 def count_rows(path_to_csv):
     with open(path_to_csv, newline="") as csvfile:
@@ -24,12 +26,3 @@ def count_rows(path_to_csv):
             i += 1
         return i
 
-def write_error_rows(path_to_errors_csv, error_rows):
-    """Write the row indices of rows with errors.
-    Input: list of row indices with errors
-    Output: csv file with each item of dictionary on its own line.
-    """
-    with open("row_errors.csv", "w+", newline="") as csvfile:
-        row_error_writer = csv.writer(csvfile, delimiter=",")
-        for ind in error_rows:
-            row_error_writer.writerow([ind])
