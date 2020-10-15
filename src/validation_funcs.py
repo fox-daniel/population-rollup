6a158e01e786c3425735cecb056d0814ce84cb75
Daniel Fox <danielfox@thoughtstoodefinite.com>
2020-10-15 15:57:25 -0400

    add docstring to geoid concat test func

--- src/validation_funcs.py
+++ src/validation_funcs.py
@@ -1,6 +1,7 @@
 import csv
 
 def test_geoid_concat(path_to_csv, path_to_log, cols, cols_inds):
+	"""This tests whether GEOID is the concatenation of ST10+COU1-+TRACT10."""
 	cols_concat = ['GEOID', 'ST10', 'COU10', 'TRACT10', 'CBSA09', 'CBSA_T', 'POP00']
 
 	with open(path_to_csv, newline = '') as csvfile:
@@ -26,3 +27,20 @@ def count_rows(path_to_csv):
             i += 1
         return i
 
+
+def test_row_length(path_to_csv, path_to_log, cols = None, cols_inds = None):
+	
+	with open(path_to_csv, newline = '') as csvfile:
+		censusreader = csv.reader(csvfile, delimiter = ',')
+		with open(path_to_log, mode = "w", newline = "") as logfile:
+			geowriter = csv.writer(logfile, delimiter=",")
+			i = 0
+			# move to the first row with data
+			row = next(censusreader)
+			for row in censusreader:
+				new_row = [row[cols_inds['GEOID']],\
+				row[cols_inds['ST10']]+row[cols_inds['COU10']]+row[cols_inds['TRACT10']]]
+				if new_row[0] != new_row[1]:
+					geowriter.writerow(row)
+				i += 1
+			geowriter.writerow(['final row checked: {0}'.format(i)])
