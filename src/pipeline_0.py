f414376a4132b747138e0c8b6cd3364ddffce685
Daniel Fox <danielfox@thoughtstoodefinite.com>
2020-10-16 11:24:03 -0400

    pipeline_0 works with missing CBSA fill and column selection

--- src/pipeline_0.py
+++ src/pipeline_0.py
@@ -15,8 +15,9 @@ path_to_log_validate_0 = "./log_validate_0.csv"
 path_to_log_validate_1 = "./log_validate_1.csv"
 path_to_log_validate_2 = "./log_validate_2.csv"
 
-path_to_transformed_1 = "./transformed_data/transformed_1.csv"
-# path_to_selected_columns = "./transformed_data/transformed_1.csv"
+path_to_filled = "./transformed_data/filled.csv"
+path_to_log_filled = "./logs/log_filled.csv"
+path_to_selected_columns = "./transformed_data/selected_columns.csv"
 
 path_to_test_1 = "../insight_testsuite/tests/test_1/input/censustract-00-10.csv"
 
@@ -27,12 +28,11 @@ cols, cols_inds = transformation_funcs.create_column_dicts(path_to_raw)
 
 selected_columns = ["GEOID", "CBSA09", "CBSA_T", "POP00", "POP10", "PPCHG"]
 
-
+# validation 0
 validation_funcs.test_geoid_concat(path_to_raw, path_to_log_validate_0, cols, cols_inds)
-
 validation_funcs.test_row_length(path_to_raw, path_to_log_validate_0)
 
-# insert: use GEOID to replace missing CBSA09s
-transformation_funcs.fill_missing_cbsa(path_to_raw, path_to_transformed_1, path_to_log_transform_1, cols_inds)
-
-transformation_funcs.select_columns(path_to_raw, path_to_transformed_1, selected_columns, cols, cols_inds)
+# transform 1
+transformation_funcs.fill_missing_cbsa(path_to_raw, path_to_filled, path_to_log_filled, cols_inds)
+transformation_funcs.select_columns(path_to_filled, path_to_selected_columns, selected_columns, cols, cols_inds)
+# transformation_funcs.convert_types()
