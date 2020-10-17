09641bd40bc86585c3076db1f0d0548359c26801
Daniel Fox <danielfox@thoughtstoodefinite.com>
2020-10-17 16:06:52 -0400

    add commands to explore pwd

--- run.sh
+++ run.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# cp ./insight_testsuite/tests/test_1/input/censustract-00-10.csv ./input/
 echo "The pwd is:"
 pwd
-echo "This directory has the following:"
-ls -a
 echo "The input directory has:"
 ls ./input/
-echo "The run.sh file looks like:"
-cat run.sh
 echo "The src dir has the following:"
 ls ./src/
+
 python3.7 ./src/pipeline.py ./input/censustract-00-10.csv ./output/report.csv
 
-# rm ./input/censustract-00-10.csv
