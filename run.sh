d3748a63c86aa2cc626891b121eb86d47e030706
Daniel Fox <danielfox@thoughtstoodefinite.com>
2020-10-17 14:36:22 -0400

    in run.sh add command to copy an input file to input

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 echo "The pwd according to run.sh is ${PWD}"
-python3.7 ./src/pipeline.py ../input/censustract-00-10.csv ../output/report.csv
+
+# Test_1
+cp ./insight_testsuite/tests/test_1/input/censustract-00-10.csv ./input/
+python3.7 ./src/pipeline.py ./input/censustract-00-10.csv ./output/report.csv
+# rm ./input/censustract-00-10.csv
