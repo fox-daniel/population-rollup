efa5a69f3917aeaae5437f780b294693b17e950a
Daniel Fox <danielfox@thoughtstoodefinite.com>
2020-10-17 16:00:45 -0400

    add pwd and ls to run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # cp ./insight_testsuite/tests/test_1/input/censustract-00-10.csv ./input/
+echo "The pwd is:"
 pwd
+echo "This directory has the following:"
+ls -a
 python3.7 ./src/pipeline.py ./input/censustract-00-10.csv ./output/report.csv
 
 # rm ./input/censustract-00-10.csv
