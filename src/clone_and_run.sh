467572954b7d1b7c98237c3b26e826933c263ede
Daniel Fox <danielfox@thoughtstoodefinite.com>
2020-10-17 15:49:02 -0400

    revise .sh files

--- src/clone_and_run.sh
+++ src/clone_and_run.sh
@@ -5,7 +5,7 @@ git clone https://github.com/fox-daniel/population-rollup pop_roll_test
 cd ./pop_roll_test
 
 # Test_1
-cp ./insight_testsuite/tests/test_1/input/censustract-00-10.csv ./input/
+# cp ./insight_testsuite/tests/test_1/input/censustract-00-10.csv ./input/
 
 ./run.sh
 
