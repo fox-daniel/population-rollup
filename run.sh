588bc2b72f7df2d814354e8c220122df77d5977f
Daniel Fox <danielfox@thoughtstoodefinite.com>
2020-10-17 15:54:38 -0400

    remove cp command from run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp ./insight_testsuite/tests/test_1/input/censustract-00-10.csv ./input/
+# cp ./insight_testsuite/tests/test_1/input/censustract-00-10.csv ./input/
 
 python3.7 ./src/pipeline.py ./input/censustract-00-10.csv ./output/report.csv
 
