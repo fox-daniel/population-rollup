4199c165d32a4a7c95e35d2fb53e80ea1f198e95
Daniel Fox <danielfox@thoughtstoodefinite.com>
2020-10-17 16:23:38 -0400

    cat complaints.csv

--- run.sh
+++ run.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-echo "The pwd is:"
-pwd
-echo "The input directory has:"
-ls ./input/
-echo "The src dir has the following:"
-ls ./src/
+cat ./input/complaints.csv
 
 python3.7 ./src/pipeline.py ./input/complaints.csv ./output/report.csv
 
