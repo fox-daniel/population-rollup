8a76f99bca364a1598486a4874954d45de48ee57
Daniel Fox <danielfox@thoughtstoodefinite.com>
2020-10-17 15:27:39 -0400

    comment out rm census file from input

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ cp ./insight_testsuite/tests/test_1/input/censustract-00-10.csv ./input/
 
 python3.7 ./src/pipeline.py ./input/censustract-00-10.csv ./output/report.csv
 
-rm ./input/censustract-00-10.csv
+# rm ./input/censustract-00-10.csv
