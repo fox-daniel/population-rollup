d2b6b5ad08997c9b4327f8131f94f03ba6847790
Daniel Fox <danielfox@thoughtstoodefinite.com>
2020-10-17 13:23:33 -0400

    revise path var in run.sh to reference src as pwd

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "The pwd according to run.sh is ${PWD}"
-python3.7 ./src/pipeline.py ./input/censustract-00-10.csv ./output/report.csv
+python3.7 ./src/pipeline.py ../input/censustract-00-10.csv ../output/report.csv
