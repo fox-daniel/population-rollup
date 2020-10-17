0698ee30b02aed5d5c4e0e35782178075e3bcd2f
Daniel Fox <danielfox@thoughtstoodefinite.com>
2020-10-17 13:19:58 -0400

    revise run.sh and clone_and_run.sh

--- src/clone_and_run.sh
+++ src/clone_and_run.sh
@@ -4,5 +4,7 @@ git clone https://github.com/fox-daniel/population-rollup pop_roll_test
 
 
 echo "****The pwd is ${PWD}.****"
-
-./pop_roll_test/run.sh
+cd ./pop_roll_test
+echo "****After cd to repo, the pwd is ${PWD}.****"
+echo "Now ./run.sh"
+./run.sh
