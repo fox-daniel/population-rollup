a78fc751f34fcceb644b1cee71baccdd5e38cafe
Daniel Fox <danielfox@thoughtstoodefinite.com>
2020-10-18 10:37:32 -0400

    changed log_groupy to just log that will be used for all errors

--- src/pipeline.py
+++ src/pipeline.py
@@ -15,7 +15,7 @@ path_to_raw = args.path_to_input_file
 path_to_report = args.path_to_output_file
 path_to_selected_columns = "./src/transformed_data/selected_columns.csv"
 path_to_cleaned_types = "./src/transformed_data/cleaned_types.csv"
-path_to_log_groupby = "./src/logs/log_groupby.csv"
+path_to_log = "./src/logs/log.csv"
 
 
 # dictionaries {column_index, column_name} & {column_name, column_index}
@@ -45,7 +45,7 @@ transformation.clean_types(
     ppchg_avg,
     error_rows,
 ) = transformation.groupby_cbsa(
-    path_to_cleaned_types, path_to_log_groupby, selected_columns, cols, cols_inds
+    path_to_cleaned_types, path_to_log, selected_columns, cols, cols_inds
 )
 
 transformation.write_report(
