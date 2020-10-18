dee495ddc2e25716531755ff3618f54e792cc418
Daniel Fox <danielfox@thoughtstoodefinite.com>
2020-10-18 10:21:26 -0400

    remove unused imports and path variables from pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,13 +1,10 @@
-import csv
+# import csv
 import importlib
 import argparse
-import development
 import validation
 import transformation
 
-importlib.reload(validation)
 importlib.reload(transformation)
-importlib.reload(development)
 
 parser=argparse.ArgumentParser("Accept input and output file paths.")
 parser.add_argument("path_to_input_file", help="The input path should be './input/censustract-00-10.csv'.", type=str)
@@ -17,19 +14,9 @@ args = parser.parse_args()
 # file paths
 path_to_raw = args.path_to_input_file
 path_to_report = args.path_to_output_file
-
-path_to_log_validate_0 = "./src/logs/log_validate_0.csv"
-path_to_log_validate_1 = "./src/logs/log_validate_1.csv"
-path_to_log_validate_2 = "./src/logs/log_validate_2.csv"
-
-path_to_filled = "./src/transformed_data/filled.csv"
-path_to_log_filled = "./src/logs/log_filled.csv"
 path_to_selected_columns = "./src/transformed_data/selected_columns.csv"
 path_to_cleaned_types = "./src/transformed_data/cleaned_types.csv"
 path_to_log_groupby = "./src/logs/log_groupby.csv"
-
-path_to_test_1 = "./insight_testsuite/tests/test_1/input/censustract-00-10.csv"
-
 
 
 # dictionaries {column_index, column_name} & {column_name, column_index}
@@ -37,10 +24,6 @@ cols, cols_inds = transformation.create_column_dicts(path_to_raw)
 
 selected_columns = ["GEOID", "CBSA09", "CBSA_T", "POP00", "POP10", "PPCHG"]
 col_types = ["int", "int", "str", "int", "int", "float"]
-
-# validation 0
-# validation.test_geoid_concat(path_to_raw, path_to_log_validate_0, cols, cols_inds)
-# validation.test_row_length(path_to_raw, path_to_log_validate_0)
 
 # transform 1
 transformation.select_columns(
