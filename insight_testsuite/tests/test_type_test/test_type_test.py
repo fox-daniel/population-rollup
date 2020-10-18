cfed1d52725a9df57f97cd87e4018903ea2eab8d
Daniel Fox <danielfox@thoughtstoodefinite.com>
2020-10-18 18:03:46 -0400

    add test_type_test.py

---
+++ insight_testsuite/tests/test_type_test/test_type_test.py
@@ -0,0 +1,25 @@
+import csv
+import sys
+
+sys.path.append("./../../../src/")
+import importlib
+import validation
+import transformation
+
+importlib.reload(validation)
+importlib.reload(transformation)
+
+path_to_raw = "./input/test_input.csv"
+path_to_log = "./test_log.csv"
+
+
+def main():
+    """Test the type of each value of each row."""
+    # dictionaries {column_index, column_name} & {column_name, column_index}
+    col_types = [int, int, int, int, float, float, int, int, str, any, int, str, int, int, int, int, int, float, int, float]
+    cols, cols_inds = transformation.create_column_dicts(path_to_raw)
+    validation.test_types(path_to_raw, path_to_log, cols_inds, col_types)
+
+
+if __name__ == "__main__":
+    main()
