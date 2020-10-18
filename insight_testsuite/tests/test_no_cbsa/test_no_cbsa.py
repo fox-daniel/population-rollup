0964cdd4e91a352b7c3afa69bd178a1ba5f375e6
Daniel Fox <danielfox@thoughtstoodefinite.com>
2020-10-18 19:22:42 -0400

    add test_no_cbsa

---
+++ insight_testsuite/tests/test_no_cbsa/test_no_cbsa.py
@@ -0,0 +1,47 @@
+import csv
+import sys
+
+sys.path.append("./../../../src/")
+import importlib
+import validation
+import transformation
+
+importlib.reload(validation)
+importlib.reload(transformation)
+
+path_to_raw = "./input/test_input.csv"
+path_to_selected_columns = "./output/transformed.csv"
+path_to_report = "./output/report.csv"
+path_to_log = "./test_log.csv"
+
+
+def main():
+    """Test that rows with no cbsa do not contribute to output."""
+    # dictionaries {column_index, column_name} & {column_name, column_index}
+    selected_columns = ["GEOID", "CBSA09", "CBSA_T", "POP00", "POP10", "PPCHG"]
+    cols, cols_inds = transformation.create_column_dicts(path_to_raw)
+    transformation.select_columns(
+        path_to_raw, path_to_selected_columns, selected_columns, cols_inds
+    )
+
+    cols, cols_inds = transformation.create_column_dicts(path_to_selected_columns)
+    (
+        cbsa_title,
+        tract_count,
+        pop00_count,
+        pop10_count,
+        ppchg_avg,
+        error_rows,
+    ) = transformation.groupby_cbsa(path_to_selected_columns, path_to_log, selected_columns, cols_inds)
+
+    transformation.write_report(
+    path_to_report,
+    cbsa_title,
+    tract_count,
+    pop00_count,
+    pop10_count,
+    ppchg_avg,
+	)
+
+if __name__ == "__main__":
+    main()
