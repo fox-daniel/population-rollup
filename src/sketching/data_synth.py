123b1cf614b5aad199214fa3838f7b24cc281bca
Daniel Fox <danielfox@thoughtstoodefinite.com>
2020-10-19 09:06:17 -0400

    data_synth.py writes to csv

--- src/sketching/data_synth.py
+++ src/sketching/data_synth.py
@@ -4,6 +4,8 @@ import csv
 # GEOID,ST10,COU10,TRACT10,AREAL10,AREAW10,CSA09,CBSA09,CBSA_T,MDIV09,CSI,COFLG,POP00,HU00,POP10,HU10,NPCHG,PPCHG,NHCHG,PHCHG
 # 01001020100,01,001,020100,3.787640715,0.01402014218,388,33860,"Montgomery, AL",,1,O,1906,764,1912,752,6,0.31,-12,-1.57
 
+path_to_ouput = "./synthdata.csv"
+
 col_names = ["GEOID","ST10","COU10","TRACT10","AREAL10","AREAW10","CSA09","CBSA09","CBSA_T","MDIV09","CSI","COFLG","POP00","HU00","POP10","HU10","NPCHG","PPCHG","NHCHG","PHCHG"]
 col_specs = [(int, 11), (int, 2), (int, 3), (int, 6), (float, None), (float, None), (int, 3), (int, 5), (str, None), (any, None), (int, None), (str, None), (int, None), (int, None), (int, None), (int, None), (int, None), (float, None), (int, None), (float, None)]
 name_specs = dict(zip(col_names, col_specs))
@@ -27,8 +29,9 @@ arstack = np.hstack(arlist)
 
 print(arstack)
 
-# geo_column = np.random.randint(10**10,10**11-1, (4,1))
-# st10_column = np.random.randint(10,10**2-1, (4,1))
 
-
-# print(ar[3,0])
+with open(path_to_ouput, "w", newline="") as synthdata:
+    synthwriter = csv.writer(synthdata, delimiter=",")
+    synthwriter.writerow(col_names)
+    for i in range(arstack.shape[0]):
+    	synthwriter.writerow(arstack[i,:])
