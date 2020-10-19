be9f9cacd7b5907d9a267e627994e8dc7f888e74
Daniel Fox <danielfox@thoughtstoodefinite.com>
2020-10-19 09:25:00 -0400

    data_synth.py switch to row first creation

--- src/sketching/data_synth.py
+++ src/sketching/data_synth.py
@@ -7,31 +7,31 @@ import csv
 path_to_ouput = "./synthdata.csv"
 
 col_names = ["GEOID","ST10","COU10","TRACT10","AREAL10","AREAW10","CSA09","CBSA09","CBSA_T","MDIV09","CSI","COFLG","POP00","HU00","POP10","HU10","NPCHG","PPCHG","NHCHG","PHCHG"]
-col_specs = [(int, 11), (int, 2), (int, 3), (int, 6), (float, None), (float, None), (int, 3), (int, 5), (str, None), (any, None), (int, None), (str, None), (int, None), (int, None), (int, None), (int, None), (int, None), (float, None), (int, None), (float, None)]
+col_specs = [(int, 11), (int, 2), (int, 3), (int, 6), (float, None), (float, None), (int, 3), (int, 5), (str, None), ('any', None), (int, None), (str, None), (int, None), (int, None), (int, None), (int, None), (int, None), (float, None), (int, None), (float, None)]
 name_specs = dict(zip(col_names, col_specs))
-
-num_rows = 4
-name = "AREAL10"
-print(int == name_specs[name][0])
-arlist = []
-for name in name_specs.keys():
-	if (name_specs[name][0] == int) and (name_specs[name][1] is not None):
-		num_min = 10**(name_specs[name][1]-1)
-		num_max = 10**name_specs[name][1]-1
-		ar = np.random.randint(num_min, num_max, (num_rows, 1))
-		arlist.append(ar)
-	elif (name_specs[name][0] == int) and (name_specs[name][1] is None):
-		num_min = 0
-		num_max = 10**5
-		ar = np.random.randint(num_min, num_max, (num_rows, 1))
-		arlist.append(ar)
-arstack = np.hstack(arlist)
-
-print(arstack)
-
 
 with open(path_to_ouput, "w", newline="") as synthdata:
     synthwriter = csv.writer(synthdata, delimiter=",")
     synthwriter.writerow(col_names)
-    for i in range(arstack.shape[0]):
-    	synthwriter.writerow(arstack[i,:])
+    num_rows = 4
+    for i in range(num_rows):
+        row = []
+        for name in name_specs.keys():
+            if (name_specs[name][0] == int) and (name_specs[name][1] is not None):
+                num_min = 10**(name_specs[name][1]-1)
+                num_max = 10**name_specs[name][1]-1
+                value = np.random.randint(num_min, num_max)
+            elif (name_specs[name][0] == int) and (name_specs[name][1] is None):
+                num_min = 0
+                num_max = 10**5
+                value = np.random.randint(num_min, num_max)
+            elif (name_specs[name][0] == float):
+                num_min = 0
+                num_max = 10**4
+                value = num_max * np.random.random()
+            elif (name_specs[name][0] == str):
+                value = "some, string"
+            elif (name_specs[name][0] == 'any'):
+                value = "mdiv09 value"
+            row.append(value)
+        synthwriter.writerow(row)
